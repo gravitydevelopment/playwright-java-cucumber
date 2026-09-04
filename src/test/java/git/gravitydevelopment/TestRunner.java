918860e11fb8511aa50952278002eee6004a13cb
fami <gravitydevelopment@gmail.com>
2026-09-05 07:37:42 +0800

    add skeleton classpath and pom

---
+++ src/test/java/git/gravitydevelopment/TestRunner.java
@@ -0,0 +1,9 @@
+package git.gravitydevelopment;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+public class TestRunner {
+}
